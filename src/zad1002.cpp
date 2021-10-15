4e213b7e79e46e7fdd37ef0b9035388ae0a99f72
Student <s25090@pjwstk.edu.pl>
2021-10-15 20:24:45 +0200

    zad 2

---
+++ src/zad1002.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+auto ask_user_for_integer(std::string const prompt) -> int;
+
+
+auto main() -> int
+{
+	int liczba1,liczba2;
+	liczba1= ask_user_for_integer("wprowadz pierwsza liczbe: ");
+	liczba2= ask_user_for_integer("wprowadz druga liczbe: ");
+	std::cout << "suma liczb: " << liczba1+liczba2 << std::endl;
+}
+
+
+
+
+
+auto ask_user_for_integer(std::string const prompt) -> int
+{
+	if (not prompt.empty()) {
+	    std:: cout << prompt;
+	}
+	auto value = std::string{};
+	std::getline(std::cin, value);
+	return std::stoi(value);
+}
+
