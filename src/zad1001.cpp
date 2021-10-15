8c4a911db35ca46d986183cd083b5f07f527986e
Student <s25090@pjwstk.edu.pl>
2021-10-15 18:16:01 +0200

    zad 1

---
+++ src/zad1001.cpp
@@ -0,0 +1,12 @@
+/*
+ * The very first program you should write in any new programming language.
+ */
+
+
+#include <iostream>
+
+auto main() -> int
+{
+    std::cout << "Alicja Wosik\n";
+    return 0;
+}
