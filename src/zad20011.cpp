309ffada9cba12aea90422157fc4f6a417a726dc
Student <s25090@pjwstk.edu.pl>
2021-10-20 21:41:14 +0200

    s01

---
+++ src/zad20011.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+auto main() -> int
+{
+	int wysokosc, szerokosc;
+	cout << "Podaj wysokosc prostokata: " << endl;
+	cin >> wysokosc;
+	cout << "Podaj szerokosc prostokata: " << endl;	
+	cin >> szerokosc;
+
+	for (int w = 0; w < wysokosc; w++)
+	{
+		for (int s = 0; s < szerokosc; s++)
+		{
+			cout << '*';
+		}
+	}
+	cout << endl;
+}
+
